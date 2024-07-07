6141b2f663933f8825f01baa9d4da5c8fdc75398
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-07-06 22:33:00 -0500

    Rutas Parte 1
    Se crearon las rutas pero tienen unos detalles que ajustar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '@layout/footer/footer.component';
+import { HeaderComponent } from '@layout/header/header.component';
+import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SkeletonComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,34 @@
 import { Routes } from '@angular/router';
+import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
+import { AboutComponent } from '@modules/about/pages/about/about.component';
+import { ContactComponent } from '@modules/contact/pages/contact/contact.component';
 
 export const routes: Routes = [
-
+    {
+        path: 'app',
+        component: SkeletonComponent,
+        children: [
+            {
+                path: 'home',
+                loadChildren: () => import('@modules/home/home-routing.module').then(m => HomeComponent)
+            },
+            {
+                path: '**',
+                redirectTo: 'home'
+            }
+            // {
+            //     path: 'about',
+            //     loadChildren: () => import('@modules/about/about-routing.module').then(m => AboutComponent)
+            // },
+            // {
+            //     path: 'contact',
+            //     loadChildren: () => import('@modules/contact/contact-routing.module').then(m => ContactComponent)
+            // },
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'app'
+    }
 ];
