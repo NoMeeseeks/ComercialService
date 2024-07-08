070158599ab99f105b75b5b576beba641007cefb
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-07-07 23:07:10 -0500

    Rutas parte 2
    Ya se pudo mejorar la zona de rotas del home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,19 @@
 import { Routes } from '@angular/router';
-import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
-import { HomeComponent } from './modules/home/pages/home/home.component';
-import { AboutComponent } from '@modules/about/pages/about/about.component';
-import { ContactComponent } from '@modules/contact/pages/contact/contact.component';
 
 export const routes: Routes = [
     {
         path: 'app',
-        component: SkeletonComponent,
         children: [
             {
                 path: 'home',
-                loadChildren: () => import('@modules/home/home-routing.module').then(m => HomeComponent)
+                title: 'Menu Inicial',
+                loadComponent: () => import('@modules/home/pages/home/home.component')
             },
             {
                 path: '**',
                 redirectTo: 'home'
             }
-            // {
-            //     path: 'about',
-            //     loadChildren: () => import('@modules/about/about-routing.module').then(m => AboutComponent)
-            // },
-            // {
-            //     path: 'contact',
-            //     loadChildren: () => import('@modules/contact/contact-routing.module').then(m => ContactComponent)
-            // },
+
         ]
     },
-    {
-        path: '**',
-        redirectTo: 'app'
-    }
 ];
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,16 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  }
+export const routes: Routes = [
+
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class HomeRoutingModule { }
