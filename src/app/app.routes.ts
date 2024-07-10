f2b74e00f3f15a0608a3540fd37493e87ade613c
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-07-09 22:28:18 -0500

    Rutas parte 4 ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,18 +7,30 @@ export const routes: Routes = [
         component: SkeletonComponent,
         children: [
             {
-                path: 'admin',
-                loadChildren: () => import('@modules/admin/admin.routes').then(m => m.routes)
-            },
-            {
                 path: 'home',
                 loadChildren: () => import('@modules/home/home-routing.module').then(m => m.routes)
             },
             {
+                path: 'about',
+                loadChildren: () => import('@modules/about/about-routing.module').then(m => m.routes)
+            },
+            {
+                path: 'contact',
+                loadChildren: () => import('@modules/contact/contact-routing.module').then(m => m.routes)
+            },
+            {
                 path: '**',
-                redirectTo: 'dashboard'
+                redirectTo: 'home'
             }
 
         ]
     },
+    {
+        path: 'admin',
+        loadChildren: () => import('@modules/admin/admin.routes').then(m => m.routes)
+    },
+    {
+        path: '**',
+        redirectTo: 'web'
+    }
 ];
